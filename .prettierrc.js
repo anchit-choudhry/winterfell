a9229fe988ac11819bffb46138350ef8221e855a
Anchit <5533940+anchit-choudhry@users.noreply.github.com>
2023-04-07 16:35:38 -0400

    1. Added Flow, Prettier and Material UI 2. Updated Jest 3. Updated Webpack loaders 4. Updated Webpack plugins 5. Updated outdated packages 6. Updated ESLint configuration 7. Organized dependencies and devDependencies 8. Refactored and formatted code

---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = {
+  "trailingComma": "all",
+  "singleQuote": true,
+  "bracketSameLine": true
+};
--- src/features/counter/counterSlice.spec.js
+++ src/features/counter/counterSlice.spec.js
@@ -1,32 +1,29 @@
-import counterReducer, {
-  increment,
-  decrement,
-  incrementByAmount,
-} from './counterSlice';
+import { expect, test } from '@jest/globals';
+import counterReducer, { decrement, increment, incrementByAmount } from './counterSlice';
 
 describe('counter reducer', () => {
   const initialState = {
     value: 3,
     status: 'idle',
   };
-  it('should handle initial state', () => {
+  test('should handle initial state', () => {
     expect(counterReducer(undefined, { type: 'unknown' })).toEqual({
       value: 0,
       status: 'idle',
     });
   });
 
-  it('should handle increment', () => {
+  test('should handle increment', () => {
     const actual = counterReducer(initialState, increment());
     expect(actual.value).toEqual(4);
   });
 
-  it('should handle decrement', () => {
+  test('should handle decrement', () => {
     const actual = counterReducer(initialState, decrement());
     expect(actual.value).toEqual(2);
   });
 
-  it('should handle incrementByAmount', () => {
+  test('should handle incrementByAmount', () => {
     const actual = counterReducer(initialState, incrementByAmount(2));
     expect(actual.value).toEqual(5);
   });
